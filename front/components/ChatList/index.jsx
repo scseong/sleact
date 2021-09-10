a2f1edef7766ea85f20938c1a09e6e0ef65abaa4
scseong <goidtv@naver.com>
2021-09-10 23:03:19 +0900

    Add infinite scrolling and adjustable scrollbar

--- front/components/ChatList/index.jsx
+++ front/components/ChatList/index.jsx
@@ -1,21 +1,38 @@
 import Chat from '@components/Chat';
 import { ChatZone, Section, StickyHeader } from '@components/ChatList/styles';
-import React, { useCallback, useRef } from 'react';
+import React, { useCallback, forwardRef } from 'react';
 import { Scrollbars } from 'react-custom-scrollbars';
 
-const ChatList = ({ chatData }) => {
-  const scrollbarRef = useRef(null);
-  const onScroll = useCallback(() => {}, []);
+const ChatList = forwardRef(({ chatSections, setSize, isEmpty, isReachingEnd, scrollbarRef }) => {
+  const onScroll = useCallback(
+    (values) => {
+      if (values.scrollTop === 0 && !isReachingEnd && !isEmpty) {
+        setSize((prevSize) => prevSize + 1).then(() => {
+          scrollbarRef.current?.scrollTop(scrollbarRef.current?.getScrollHeight() - values.scrollHeight);
+        });
+      }
+    },
+    [setSize, scrollbarRef, isReachingEnd, isEmpty],
+  );
 
   return (
     <ChatZone>
       <Scrollbars autoHide ref={scrollbarRef} onScrollFrame={onScroll}>
-        {chatData?.map((chat) => (
-          <Chat key={chat.id} data={chat} />
-        ))}
+        {Object.entries(chatSections).map(([date, chats]) => {
+          return (
+            <Section className={`section-${date}`} key={date}>
+              <StickyHeader>
+                <button>{date}</button>
+              </StickyHeader>
+              {chats.map((chat) => (
+                <Chat key={chat.id} data={chat} />
+              ))}
+            </Section>
+          );
+        })}
       </Scrollbars>
     </ChatZone>
   );
-};
+});
 
 export default ChatList;
--- front/pages/DirectMessage/index.jsx
+++ front/pages/DirectMessage/index.jsx
@@ -1,47 +1,76 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useEffect, useRef } from 'react';
 import { Container, Header } from '@pages/DirectMessage/styles';
 import gravatar from 'gravatar';
 import { useParams } from 'react-router-dom';
 import useInput from '@hooks/useInput';
-import useSWR from 'swr';
+import useSWR, { useSWRInfinite } from 'swr';
 import fetcher from '@utils/fetcher';
 import ChatBox from '@components/ChatBox';
 import ChatList from '@components/ChatList';
 import axios from 'axios';
+import makeSection from '@utils/makeSection';
 
+const PAGE_SIZE = 20;
 const DirectMessage = () => {
   const { workspace, id } = useParams();
   const { data: userData } = useSWR(`/api/workspaces/${workspace}/users/${id}`, fetcher);
   const { data: myData } = useSWR(`/api/users`, null, fetcher);
   const [chat, onChangeChat, setChat] = useInput('');
-  const { data: chatData, mutate: mutateChat, revalidate } = useSWR(
-    `/api/workspaces/${workspace}/dms/${id}/chats?perPage=20&page=1`,
+  const { data: chatData, mutate: mutateChat, revalidate, setSize } = useSWRInfinite(
+    (index) => `/api/workspaces/${workspace}/dms/${id}/chats?perPage=${PAGE_SIZE}&page=${index + 1}`,
     fetcher,
   );
+  const isEmpty = chatData?.[0]?.length === 0;
+  const isReachingEnd = isEmpty || (chatData && chatData[chatData.length - 1]?.length < 20) || false;
+  const scrollbarRef = useRef(null);
 
   const onSubmitForm = useCallback(
     (e) => {
       e.preventDefault();
-      console.log(chat);
-      if (chat?.trim()) {
+      if (chat?.trim() && chatData) {
+        const savedChat = chat;
+        mutateChat((prevChatData) => {
+          prevChatData?.[0].unshift({
+            id: (chatData[0][0]?.id || 0) + 1,
+            content: savedChat,
+            SenderId: myData.id,
+            Sender: myData,
+            ReceiverId: userData.id,
+            Receiver: userData,
+            createdAt: new Date(),
+          });
+          return prevChatData;
+        }, false).then(() => {
+          setChat('');
+          if (scrollbarRef.current) {
+            scrollbarRef.current.scrollToBottom();
+          }
+        });
         axios
           .post(`/api/workspaces/${workspace}/dms/${id}/chats`, {
             content: chat,
           })
           .then(() => {
-            console.log('hello?');
             setChat('');
-            revalidate();
           })
           .catch(console.error);
       }
     },
-    [chat],
+    [chat, workspace, id, myData, userData, chatData],
   );
+
+  // 로딩 시 스크롤바 제일 아래로
+  useEffect(() => {
+    if (chatData?.length === 1) {
+      scrollbarRef.current?.scrollToBottom();
+    }
+  }, [chatData]);
 
   if (!userData || !myData) {
     return null;
   }
+
+  const chatSections = makeSection(chatData ? chatData.flat().reverse() : []);
 
   return (
     <Container>
@@ -49,7 +78,13 @@ const DirectMessage = () => {
         <img src={gravatar.url(userData.email, { s: '24px', d: 'retro' })} alt={userData.nickname} />
         <span>{userData.nickname}</span>
       </Header>
-      <ChatList chatData={chatData} />
+      <ChatList
+        chatSections={chatSections}
+        scrollbarRef={scrollbarRef}
+        setSize={setSize}
+        isEmpty={isEmpty}
+        isReachingEnd={isReachingEnd}
+      />
       <ChatBox chat={chat} onChangeChat={onChangeChat} onSubmitForm={onSubmitForm} />
     </Container>
   );
