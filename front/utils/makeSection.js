f5ebc3966cb86acffef6c95b293a0b3c77be3eb8
scseong <goidtv@naver.com>
2021-09-10 18:51:57 +0900

    Fix error caused by typos

--- front/utils/makeSection.js
+++ front/utils/makeSection.js
@@ -3,11 +3,11 @@ import dayjs from 'dayjs';
 export default function makeSection(chatList) {
   const sections = {};
   chatList.forEach((chat) => {
-    const month = dayjs(chat.createAt).format('YYYY-MM-DD');
-    if (Array.isArray(section[monthDate])) {
+    const monthDate = dayjs(chat.createAt).format('YYYY-MM-DD');
+    if (Array.isArray(sections[monthDate])) {
       sections[monthDate].push(chat);
     } else {
-      sections[monthData] = [chat];
+      sections[monthDate] = [chat];
     }
   });
   return sections;
