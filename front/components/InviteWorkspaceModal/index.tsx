2602cb87d28e4558cd506a8efb41260b6f63924f
scseong <goidtv@naver.com>
2025-06-05 23:52:14 +0900

    Feat: 사용자 초대 모달 UI

---
+++ front/components/InviteWorkspaceModal/index.tsx
@@ -0,0 +1,37 @@
+import { FC, useCallback } from "react";
+import { useParams } from "react-router";
+import Modal from "@components/Modal";
+import useInput from "@hooks/useInput";
+import { Button, Input, Label } from "@pages/SignUp/styles";
+
+interface Props {
+  show: boolean;
+  onCloseModal: () => void;
+  setShowInviteWorkspaceModal: (flag: boolean) => void;
+}
+
+const InviteWorkspaceModal: FC<Props> = ({ show, onCloseModal, setShowInviteWorkspaceModal }) => {
+  const { workspace } = useParams<{ workspace: string; channel: string }>();
+  const [newMember, onChangeNewMember] = useInput("");
+
+  const onInviteMember = useCallback(
+    (e) => {
+      e.preventDefault();
+    },
+    [newMember, workspace],
+  );
+
+  return (
+    <Modal show={show} onCloseModal={onCloseModal}>
+      <form onSubmit={onInviteMember}>
+        <Label id="member-label">
+          <span>이메일</span>
+          <Input id="member" type="email" value={newMember} onChange={onChangeNewMember} />
+        </Label>
+        <Button type="submit">초대하기</Button>
+      </form>
+    </Modal>
+  );
+};
+
+export default InviteWorkspaceModal;
